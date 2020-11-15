4f40b00ff7b79d55ba6123058fc61aeb3887434a
Hien Cao <caoduonghien@gmail.com>
2020-11-15 12:24:05 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          How to learn reactt
         </a>
       </header>
     </div>
